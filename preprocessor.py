f23a0d4d6e43b863f4cbe3a58ec39bc47623fc1a
Altair-Alpha <zhangyining0626@126.com>
2022-02-13 17:12:08 +0800

    添加数据集分割等辅助功能

--- preprocessor.py
+++ preprocessor.py
@@ -1,17 +1,25 @@
+from typing import Dict, List
 import numpy
+import numpy as np
 import pandas as pd
 import os
 from stockstats import StockDataFrame
 
 
-def load_and_preprocess(path:str) -> pd.DataFrame:
+def load_and_preprocess(path: str) -> pd.DataFrame:
+    """加载原始股价数据csv文件，并添加技术指标数据。
+
+    :param path: 数据文件路径
+    :return: 添加技术指标列后的DataFrame
+    """
     stock_data = pd.read_csv(path, index_col=0)
     # print(stock_data.info())
     stock_data = stock_data[['ts_code', 'trade_date', 'open', 'high', 'low', 'close', 'vol']]
-    stock_data = add_technical_indicator(stock_data)
+    stock_data = _add_technical_indicator(stock_data)
     return stock_data
 
-def add_technical_indicator(stock_data:pd.DataFrame) -> pd.DataFrame:
+
+def _add_technical_indicator(stock_data: pd.DataFrame) -> pd.DataFrame:
     stock_data_indi = stock_data.copy()
 
     # 分离20只股票，分别计算每只股票的MACD, KDJ(K线和D线), RSI指标
@@ -20,11 +28,11 @@ def add_technical_indicator(stock_data:pd.DataFrame) -> pd.DataFrame:
     macd = pd.Series()
     kdjk = pd.Series()
     kdjd = pd.Series()
-    rsi  = pd.Series()
+    rsi = pd.Series()
 
     for k in stock_data_dict.keys():
         one_stock = stock_data_dict[k]
-        one_stock.rename({'trade_date': 'date', 'vol': 'volume'}, axis=1) # 适配stockstats库做一些列命名调整
+        one_stock.rename({'trade_date': 'date', 'vol': 'volume'}, axis=1)  # 适配stockstats库做一些列命名调整
         one_stock = StockDataFrame.retype(one_stock)
 
         one_stock_macd = one_stock['macd']
@@ -43,6 +51,50 @@ def add_technical_indicator(stock_data:pd.DataFrame) -> pd.DataFrame:
     stock_data_indi['kdjk'] = kdjk
     stock_data_indi['kdjd'] = kdjd
     stock_data_indi['rsi'] = rsi
-    stock_data_indi.fillna(method='bfill', inplace=True) # 填充每只股票第一天交易数据缺少的技术指标
-
+    stock_data_indi.fillna(method='bfill', inplace=True)  # 填充每只股票第一天交易数据缺少的技术指标
     return stock_data_indi
+
+
+def subdata_by_range(data: pd.DataFrame, start_date: int, end_date: int) -> pd.DataFrame:
+    """通过开始日期和结束日期截取数据集的子集
+
+    :param data: 原始数据
+    :param start_date: 开始日期
+    :param end_date: 结束日期
+    """
+    sub_data = data[(data['trade_date'] >= start_date) & (data['trade_date'] <= end_date)].reset_index(drop=True)
+    return sub_data
+
+
+def subdata_by_ndays(data: pd.DataFrame, n_days: int, start_date: int = 0) -> pd.DataFrame:
+    """通过开始日期和交易天数截取数据集的子集。如果不声明开始日期，则默认从原数据开头开始截取。请确保开始日期存在于原始数据中
+
+    :param data: 原始数据
+    :param n_days: 截取交易天数
+    :param start_date: 开始日期
+    """
+    dates = list(data['trade_date'].unique())
+    index = 0 if (start_date == 0) else dates.index(start_date)
+    target_dates = dates[index : index+n_days]
+    # print('DATES')
+    # print(target_dates)
+    sub_data = data[data['trade_date'].isin(target_dates)].reset_index(drop=True)
+    return sub_data
+
+
+def to_daily_data(data: pd.DataFrame) -> Dict[int, pd.DataFrame]:
+    """将所有股票所有日期的DataFrame分割为日期为键，当日所有股票数据DataFrame为值的字典
+
+    :param data: 原始数据
+    """
+    daily_data = dict(tuple(data.groupby('trade_date')))
+    for v in daily_data.values():
+        v.reset_index(inplace=True)
+    return daily_data
+
+def get_stock_codes(data: pd.DataFrame) -> List[str]:
+    """获取原始数据中所有股票代码的list
+
+    :param data: 原始数据
+    """
+    return list(data['ts_code'].unique())
