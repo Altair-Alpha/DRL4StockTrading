43312224c35a58f128072e5b36f8db6425820c64
Altair-Alpha <zhangyining0626@126.com>
2022-02-13 17:42:53 +0800

    添加兼容OpenAI gym接口的股市模拟环境Ver1

---
+++ rl_env/stock_train_env.py
@@ -0,0 +1,187 @@
+from typing import List
+from typing import Dict
+import numpy as np
+import pandas as pd
+from datetime import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+import gym
+from gym import spaces
+
+# 股市环境全局变量，read_config()中从文件读取数值
+
+INITIAL_BALANCE = 0             # 初始资金
+STOCK_DIM = 0                   # 股票个数
+SHARES_PER_TRADE = 0            # 每笔交易股数单位（因数）
+TRANSACTION_FEE_PERCENTAGE = 0  # 交易费率（百分比）
+
+sep_line1 = '================================================================'
+sep_line2 = '----------------------------------------------------------------'
+
+class StockTrainEnvV1(gym.Env):
+    """符合OpenAI Gym接口的模拟股市环境"""
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self, daily_data: Dict[int, pd.DataFrame], stock_codes: List[str], verbose: bool=True):
+        super(StockTrainEnvV1, self).__init__()
+
+        self.verbose = verbose
+
+        # 动作空间，共 STOCK_DIM 维，代表对每只股票的买入/卖出行为
+        # 标准化到 [-1, 1] 区间
+        self.action_space = spaces.Box(low=-1, high=1, shape=(STOCK_DIM,))
+        # 状态空间，共 1（资金）+ 6（股价+持股+MACD+KDJK+KDJD+RSI）* STOCK_DIM（股票个数）维
+        # 此问题中无法对状态空间进行标准化
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(1 + 6*STOCK_DIM,))
+
+        # stock_codes 必须与 daily_data 中股票的顺序对应
+        self.stock_codes = stock_codes
+        self.data = daily_data
+        self.dates = sorted(list(self.data.keys()))
+
+        self.reset()
+
+    def step(self, action: np.ndarray):
+        if not self.done:
+            action = action * SHARES_PER_TRADE
+
+            # 总资产为：余额 + 每只股票持股 * 当前日期股价
+            begin_total_asset = self.state[0] + sum(
+                [shares*price for shares, price in zip(self.state[1: STOCK_DIM+1],
+                                                       self.state[STOCK_DIM+1: 2*STOCK_DIM+1])])
+            self.asset_memory.append(self.state[0 : STOCK_DIM+1] + [begin_total_asset]) # state中 0至STOCK_DIM 项为（余额，持股）
+
+            if self.verbose:
+                print(sep_line1)
+                print('StockTrainEnvV1:', 'step begin, today:', self.cur_date)
+                print('StockTrainEnvV1:', 'before trade, total asset:', begin_total_asset)
+
+            # 进行交易
+            for index, one_stock_action in enumerate(action):
+                if one_stock_action > 0:
+                    self._buy_stock(index, one_stock_action)
+                elif one_stock_action < 0:
+                    self._sell_stock(index, one_stock_action)
+
+            # 更新日期
+            self.day_count += 1
+            self.cur_date = self.dates[self.day_count]
+            self.done = (self.cur_date == self.last_date)
+
+            # 取新一日的股票数据，更新state
+            self.state = self.state[0 : 1+STOCK_DIM] + self.data[self.cur_date]['close'].tolist() \
+                         + self.data[self.cur_date]['macd'].tolist() \
+                         + self.data[self.cur_date]['kdjk'].tolist() \
+                         + self.data[self.cur_date]['kdjd'].tolist() \
+                         + self.data[self.cur_date]['rsi'].tolist()
+
+            # 结束时的资产计算方式相同，此时使用的state已更新
+            end_total_asset = self.state[0] + sum(
+                [shares*price for shares, price in zip(self.state[1: STOCK_DIM+1],
+                                                       self.state[STOCK_DIM+1: 2*STOCK_DIM+1])])
+            self.reward = end_total_asset - begin_total_asset
+            self.reward_memory.append(self.reward)
+
+            if self.verbose:
+                print(sep_line1)
+                print('StockTrainEnvV1:', 'after trade, state:', self.state)
+                print('StockTrainEnvV1:', 'after trade, total asset:', begin_total_asset, end_total_asset)
+                print('StockTrainEnvV1:', 'reward:', self.reward)
+        else:
+            save_result(self.dates, self.asset_memory, self.reward_memory)
+
+        return np.array(self.state), self.reward, self.done, {}
+
+    def reset(self):
+        self.done = False
+        self.reward = 0
+        self.cur_date = self.dates[0]
+        self.last_date = self.dates[-1]
+        if self.verbose:
+            print('StockTrainEnvV1:', 'reset:', 'trade date from', self.cur_date, 'to', self.last_date)
+        self.day_count = 0
+
+        # 初始状态为：[初始资金, 持股数（全为0）, 第一天的股价数据，第一天的股价指标]
+        self.state = [INITIAL_BALANCE] + [0] * STOCK_DIM + self.data[self.cur_date]['close'].tolist() \
+                     + self.data[self.cur_date]['macd'].tolist() \
+                     + self.data[self.cur_date]['kdjk'].tolist() \
+                     + self.data[self.cur_date]['kdjd'].tolist() \
+                     + self.data[self.cur_date]['rsi'].tolist()
+        if self.verbose:
+            print('StockTrainEnvV1:', 'reset:', 'init state', self.state)
+        self.asset_memory = []  # 记录历史每步资产状态：资金+持股+总资产（前两项合起来）
+        self.reward_memory = [] # 记录历史每步收益
+
+        self.trade_count = 0
+        return np.array(self.state)
+
+    def _buy_stock(self, index, volume):
+        max_volume = self.state[0] // self.state[1 + STOCK_DIM + index] # 账户资金最多能购买的股数
+        real_volume = min(volume, max_volume) # 确保入参volume不超过最大能购买数，避免交易后账户资金为负
+
+        # 更新资金，减少：股价*购买股数*(1+交易费率)
+        amount = self.state[1 + STOCK_DIM + index] * real_volume  # A股印花税为卖方单向收费，其余小额双向收费项暂且不计
+        prev_balance = self.state[0]
+        self.state[0] -= amount
+        # 更新持股
+        self.state[1 + index] += real_volume
+        self.trade_count += 1
+        if self.verbose:
+            print(sep_line2)
+            print('StockTrainEnvV1:', f'trade no.{self.trade_count}', f'stock: {self.stock_codes[index]}',
+                   'type: buy', f'vol: {real_volume}', f'amount: {amount}')
+            print('StockTrainEnvV1:', f'balance drop from {prev_balance} to {self.state[0]}')
+
+    def _sell_stock(self, index, volume):
+        # 持股>0时，才能进行卖出操作
+        if self.state[1 + index] > 0:
+            real_volume = min(volume, self.state[1 + index]) # 确保入参volume不超过持股数，避免交易后持股为负
+            # 以下同_buy_stock，只是资金变化为增加而非减少
+            amount = self.state[1 + STOCK_DIM + index] * real_volume\
+                     * (1 - TRANSACTION_FEE_PERCENTAGE) # A股印花税为卖方单向收费，其余小额双向收费项暂且不计
+            prev_balance = self.state[0]
+            self.state[0] += amount
+            # 更新持股
+            self.state[1 + index] -= real_volume
+            self.trade_count += 1
+            if self.verbose:
+                print(sep_line2)
+                print('StockTrainEnvV1:', f'trade no.{self.trade_count}', f'stock: {self.stock_codes[index]}',
+                      'type: sell', f'vol: {real_volume}', f'amount: {amount}')
+                print('StockTrainEnvV1:', f'balance increase from {prev_balance} to {self.state[0]}')
+
+def read_config():
+    import configparser
+    conf = configparser.ConfigParser()
+    conf.read('../config/config.ini', encoding='utf-8')
+
+    global INITIAL_BALANCE, STOCK_DIM, TRANSACTION_FEE_PERCENTAGE, SHARES_PER_TRADE
+    INITIAL_BALANCE = int(conf.get('stock_env', 'initial_balance'))
+    STOCK_DIM = int(conf.get('stock_env', 'stock_dim'))
+    SHARES_PER_TRADE = int(conf.get('stock_env', 'shares_per_trade'))
+    TRANSACTION_FEE_PERCENTAGE = float(conf.get('stock_env', 'transaction_fee_percentage'))
+
+def save_result(dates, assets, rewards):
+    # 绘制资产变化图
+    plt.figure(figsize=(18, 6))
+    plt.margins(x=0.02)
+    x = [datetime.strptime(str(d), '%Y%m%d').date() for d in dates]
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y/%m/%d'))
+    interval = np.clip(len(dates) // 100, 1, 120)
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=interval))
+    plt.plot(x, [a[-1] for a in assets]) # assets每项中最后一个值是余额+持股价值的总资产
+    # plt.plot(x, rewards)
+    plt.gcf().autofmt_xdate()
+    plt.savefig('./figs/simulation/test.png')
+
+if __name__ == '__main__':
+    from stable_baselines3.common.env_checker import check_env
+    from preprocessor import *
+    read_config()
+    data = pd.read_csv('../data/szstock_20_preprocessed.csv')
+    data = subdata_by_ndays(data, 10)
+    stock_codes = get_stock_codes(data)
+    data = to_daily_data(data)
+    env = StockTrainEnvV1(data, stock_codes)
+    check_env(env, warn=True)
